926766755463375dd16bd13d4284c1f71f456eca
JusRecondo <justina.recondo@gmail.com>
2025-08-17 20:47:10 -0300

    apply light theme as default

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={geist.className}>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system">
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           <SearchProvider>
             <FiltersProvider>
               <NewsProvider>
